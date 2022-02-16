6d38795b6c74622a2ebbb67086d27afb37a7dfba
Alex Alikhani <alikhanialex@gmail.com>
2022-02-16 20:53:16 +0000

    Handling state of people

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+interface PeopleState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[]
+}
+
+const App = () => {
+  const [people, setPeople] = useState<PeopleState["people"]>([])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to the Party</h1>
     </div>
   );
-}
+};
 
 export default App;
